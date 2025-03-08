201736cd6c3a45985778d9d6ca94a4dcd2623958
Sanchez-C <corentinsanchez@gmail.com>
2025-03-08 18:17:00 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const { Client } = require('pg');
 
+const connectionOptions = {
+  connectionString: process.env.DB_URL
+};
+
+// Désactiver SSL si le serveur ne le supporte pas
+if (process.env.DB_DISABLE_SSL === 'true') {
+  connectionOptions.ssl = false;
+} else {
+  connectionOptions.ssl = { rejectUnauthorized: false };
+}
+
 // Création d'une instance de client PostgreSQL avec les variables d'environnement définies sur Render
-const client = new Client({
-  connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const client = new Client(connectionOptions);
 
 // Connexion à la base de données
 client.connect()
