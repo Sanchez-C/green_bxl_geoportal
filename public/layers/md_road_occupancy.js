0effce552d5325034e79783fba27d9f9eeefdc1f
Sanchez-C <corentinsanchez@gmail.com>
2025-03-09 15:39:57 +0100

    Update md_road_occupancy.js

--- public/layers/md_road_occupancy.js
+++ public/layers/md_road_occupancy.js
@@ -4,7 +4,7 @@ var lyr_md_road_occ_morning = new ol.layer.Vector({
         strategy: ol.loadingstrategy.bbox,
         loader: function (extent, resolution, projection) {
           // Teste la disponibilité du service WFS
-          var apiUrl = "http://localhost:3000/api/road_occupancy";
+          var apiUrl = "https://green-brussels.onrender.com/api/road_occupancy";
           var wfsUrl = "http://localhost:8080/geoserver/green_brussels/wfs?service=WFS&version=1.1.0" +
                     "&request=GetFeature&typeName=green_brussels:md_road_occupancy" +
                     "&outputFormat=application/json&srsname=EPSG:31370";  
@@ -74,7 +74,7 @@ var lyr_md_road_occ_evening = new ol.layer.Vector({
         strategy: ol.loadingstrategy.bbox,
         loader: function (extent, resolution, projection) {
           // Teste la disponibilité du service WFS
-          var apiUrl = "http://localhost:3000/api/road_occupancy";
+          var apiUrl = "https://green-brussels.onrender.com/api/road_occupancy";
           var wfsUrl = "http://localhost:8080/geoserver/green_brussels/wfs?service=WFS&version=1.1.0" +
                     "&request=GetFeature&typeName=green_brussels:md_road_occupancy" +
                     "&outputFormat=application/json&srsname=EPSG:31370";  
